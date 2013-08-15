398be9154eb5da9c87a3d50b1a251516f615249b
Roby Van Damme <roby.vandamme@irobyx.com>
2013-08-15 12:55:38 +0200

    Added NUnit testproject with sample test

---
+++ VS2012/TestBuild/TestBuild.UnitTests/SampleTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TestBuild.UnitTests
+{
+    [TestFixture]
+    public class SampleTest
+    {
+        [Test]
+        public void PassingTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
